c748cd807eadd9cc30692aab542259d3f94c5760
Tito Alvarez <augustoalvarez@gmail.com>
2015-10-18 12:40:49 -0600

    New parallel testing with Docker containers, using Robo as primary orchestrator

---
+++ tests/docker/docker-entrypoint-specific.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export GITHUB_TOKEN="{github:token}"
+export SLACK_CHANNEL="{slack:channel}"
+export SLACK_TOKEN="{slack:token}"
+export DOCKERTEST_PHPVERSION="{php:version}"
+export DOCKERTEST_JOOMLAVERSION="{joomla:version}"
+export DOCKERTEST_APPNAME="{app:name}"
